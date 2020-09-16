66132d0b5e40237c696e1e84e9449ed777456f40
dbeccaria826 <beccdaniel@gmail.com>
2020-09-16 09:30:42 -0400

    searchfield: working on mobile styling for search field

--- js/index.js
+++ js/index.js
@@ -1,7 +1,20 @@
-const apiKey = 'at_jV69a6mZ1tpGAStRl8hAnhEU8BBlm'; 
-const ipAddress = '8.8.8.8'; // Search term
-const domain = 'wordpress.org'; // Search term
+// Map
+// const map = L.map('mapid').setView([51.505, -0.09], 13);
 
-fetch(`https://geo.ipify.org/api/v1?apiKey=${apiKey}&domain=${domain}`)
-  .then(response => response.json())
-  .then(json => console.log(JSON.stringify(json)));
+// L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+// }).addTo(map);
+
+// L.marker([51.5, -0.09]).addTo(map)
+//     .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+//   .openPopup();
+    
+(function () {
+  const apiKey = 'at_jV69a6mZ1tpGAStRl8hAnhEU8BBlm'; 
+  const ipAddress = '8.8.8.8'; // Search term
+  const domain = 'wordpress.org'; // Search term
+  
+  fetch(`https://geo.ipify.org/api/v1?apiKey=${apiKey}&domain=${domain}`)
+    .then(response => response.json())
+    .then(json => console.log(json));
+})()
