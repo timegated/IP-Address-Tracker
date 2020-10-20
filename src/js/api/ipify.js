b4fc4bc753a5bfb981a161901c2803ab8a3e13b6
dbeccaria826 <beccdaniel@gmail.com>
2020-10-19 20:41:56 -0400

    Replaced cities with region in response request

--- src/js/api/ipify.js
+++ src/js/api/ipify.js
@@ -15,7 +15,7 @@ export const ipSearch = async () => {
   try {
 
     const response = await axios.get(`${apiURL}${key}&domain=${ip}`);
-    ui.displayData(response.data.ip, response.data.location.city, response.data.location.city, response.data.location.postalCode, response.data.location.timezone, response.data.isp)
+    ui.displayData(response.data.ip, response.data.location.city, response.data.location.region, response.data.location.postalCode, response.data.location.timezone, response.data.isp)
 
     map = L.map('mapId').setView([response.data.location.lat, response.data.location.lng], 13);
     L.tileLayer('https://c.tile.openstreetmap.org/{z}/{x}/{y}.png', {
